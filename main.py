421bcef76f511983500cc9d4d269f2a4cd2bdc16
Keaton <keatonrproud@gmail.com>
2023-12-04 11:27:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import pyautogui as pg
 from pynput import mouse, keyboard
 import time
 
-class ClickerRunner:
+END_KEY = 'X'
+class autoClicker():
     def __init__(self, coords, single=True, click_delay=1, stop_key='X'):
         self.coords = coords
         self.single = single
+        self.run = True
         self.click_delay = click_delay
         self.stop_key = stop_key
-        self.run = True
-        self.listener = keyboard.Listener(on_release=self.on_key_release)
 
     def run_clicker(self):
+        self.listener = keyboard.Listener(on_release=self.on_key_release)
         self.listener.start()
 
         while self.run:
@@ -24,26 +25,31 @@ class ClickerRunner:
                     for y in range(y1, y2, y_dist):
                         pg.click(x, y)
             time.sleep(self.click_delay)
+        else:
+            quit()
 
     def on_key_release(self, key):
         try:
             if key.char == self.stop_key:
-                self.run = False
+                self.stop()
         except AttributeError:
             pass
 
     def stop(self):
         self.listener.stop()
-        self.listener.join()
+        self.run = False
+
 
 def on_click(x, y, _, pressed):
     if pressed:
         on_click.coords = (x, y)
         return False
 
+
 def set_on_click():
     with mouse.Listener(on_click=on_click) as listener:
         listener.join()
+
 
 def set_single_clicker():
     print(f'Click where you want the clicking to occur.')
@@ -51,6 +57,7 @@ def set_single_clicker():
     x, y = on_click.coords
     print(f'Clicking at coords ({x}, {y})')
     return x, y
+
 
 def set_area_clicker():
     print('Click your mouse in the top-left corner of the area to be clicked.')
@@ -68,18 +75,19 @@ def set_area_clicker():
 
     return x1, y1, x2, y2, x_dist, y_dist
 
+
 def main():
     try:
         delay = int(input('Number of seconds between click times (int): '))
     except ValueError:
-        print('The last submission was not an integer. The default delay of 1 second will be used.')
+        print('The last submission was not an integer. The default delay of 1 second will be used. ')
         delay = 1
 
-    stop_key = input('Submit the key you want to press to end the clicking: ')
-    if stop_key == '':
-        print("No stop key was submitted. Your stop key is 'X'.")
-        stop_key = 'X'
-    elif len(stop_key) > 1: stop_key = stop_key[0]
+    stop_key = input('Input the key to stop the clicking: [default = X] ')
+    if stop_key == '': stop_key = 'X'
+    else: stop_key = stop_key[0]
+
+    print(f'Your stop key is "{stop_key}"')
 
     single_click = input('Single-click, or click throughout an area on the screen? [Y = single-click, N = area on screen] ')
     single_click = False if single_click == 'N' else True
@@ -89,9 +97,8 @@ def main():
     print('Clicker will begin in 2 seconds.')
     time.sleep(2)
 
-    clicker = ClickerRunner(coords, single=single_click, click_delay=delay, stop_key=stop_key)
+    clicker = autoClicker(coords, single=single_click, click_delay=delay, stop_key=stop_key)
     clicker.run_clicker()
-    clicker.stop()
 
 if __name__ == '__main__':
     main()
